51aec7cec0db42ab9ed263ac5700e96b202b37ed
mireille <mimimireille95@gmail.com>
2019-08-23 06:29:20 -0400

    nav adde changes

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import {BreakpointObserver,Breakpoints,Breakpointstate } from '@angular/cdk/layout';
+import{ Observable} from 'rxjs';
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent implements OnInit {
-
-  constructor() { }
+ isHandset: Observable<Breakpointstate > = this.breakpointObserver.observe(Breakpoints.Handset);
+  constructor(private breakpointObserver:BreakpointObserver) { }
 
   ngOnInit() {
   }
